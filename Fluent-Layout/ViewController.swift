2150e2df8ec197409243706ba2a808a66290d54d
Mohammad Azam <azamsharp@gmail.com>
2017-10-04 21:37:51 -0500

    Initial Commit

---
+++ Fluent-Layout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Fluent-Layout
+//
+//  Created by Mohammad Azam on 10/4/17.
+//  Copyright © 2017 Mohammad Azam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
